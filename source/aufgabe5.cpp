cbccb9c6acab737321e8d2b6c26d0a115e00249c
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2016-05-14 11:13:51 +0200

    Bjoern ist ein lustiger Name. (Sucked@ .erase)

--- source/aufgabe5.cpp
+++ source/aufgabe5.cpp
@@ -1,16 +1,32 @@
 # define CATCH_CONFIG_RUNNER
 # include <catch.hpp>
 # include <cmath>
+# include <vector>
 # include <algorithm>
 
-TEST_CASE ("describe_factorial","[aufgabe3]")
+bool is_even(unsigned int a)
 {
-// ihre Loesung :
-// ...
-REQUIRE ( std :: all_of ( v . begin () , v . end () , is_even ));
+	return a%2==0;
 }
 
-int main (int argc, char* argv[])
+TEST_CASE ("describe_factorial","[is_even]")
 {
-return Catch :: Session (). run ( argc , argv );
+	std::vector <unsigned int> hRandNum(100);
+
+	for(auto& i : hRandNum) 
+	{
+		i = std::rand() % 101;
+	}
+	std::vector <unsigned int> evenRandNum; //weil ich fur .erase zu bjoern bin
+
+	for(auto& i : hRandNum) 
+	{
+		if(is_even(i)) evenRandNum.push_back(i) ;
+	}
+	REQUIRE ( std :: all_of ( evenRandNum.begin () , evenRandNum.end () , is_even ));
+}
+
+int main(int argc, char *argv[])
+{
+  return Catch::Session().run(argc, argv);
 }
