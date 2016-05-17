cdf812da6bb9d6c2a369e32c94370acce1254862
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2016-05-17 18:12:55 +0200

    Phil - Algorithmus implementiert und erstes Lambda geschaffen (bereitete etwas My...)

--- source/aufgabe5.cpp
+++ source/aufgabe5.cpp
@@ -9,6 +9,17 @@ bool is_even(unsigned int a)
 	return a%2==0;
 }
 
+template <typename container>
+
+	void swap(int first ,int second, container& C )
+	{
+		auto temp = C[first];
+		C[first]=C[second];
+		C[second]=temp;
+	}
+
+	
+
 TEST_CASE ("describe_factorial","[is_even]")
 {
 	std::vector <unsigned int> hRandNum(100);
@@ -24,6 +35,17 @@ TEST_CASE ("describe_factorial","[is_even]")
 		if(is_even(i)) evenRandNum.push_back(i) ;
 	}
 	REQUIRE ( std :: all_of ( evenRandNum.begin () , evenRandNum.end () , is_even ));
+}
+
+TEST_CASE ("describe_swapped","[swap]")
+{
+	std::vector <unsigned int> onetosix{1,2,3,4,5,6};
+	swap(1,4,onetosix);
+	swap(0,5,onetosix);
+	REQUIRE (onetosix[1]==5);
+	REQUIRE (onetosix[4]==2);
+	REQUIRE (onetosix[0]==6);
+	REQUIRE (onetosix[5]==1);	
 }
 
 int main(int argc, char *argv[])
--- source/aufgabe7.cpp
+++ source/aufgabe7.cpp
@@ -6,17 +6,33 @@
 # include "circle.hpp"
 
 
-TEST_CASE ("describe_factorial","[is_even]")
+TEST_CASE ("describe_sort","[is_sorted]")
 {
 	std::vector <Circle> tenDifferentCircles(10);
 
 	for(int i=0;i<10;i++) 
 	{
-		tenDifferentCircles[i].setradius(10-i);
+		tenDifferentCircles[i].setradius(rand());
 	}
 	std::sort (tenDifferentCircles.begin(), tenDifferentCircles.end());
 
 	REQUIRE (std::is_sorted(tenDifferentCircles.begin(), tenDifferentCircles.end ()));
+}
+TEST_CASE ("describe_sort2","[is_sorted]")
+{
+	std::vector <Circle> twentyDifferentCircles(20);
+
+	for(int i=0;i<10;i++) 
+	{
+		twentyDifferentCircles[i].setradius(rand());
+	}
+
+
+	std::sort (twentyDifferentCircles.begin(), twentyDifferentCircles.end(),
+		[] (Circle a, Circle b) { return a.getradius() < b.getradius(); } );
+
+REQUIRE (std::is_sorted(twentyDifferentCircles.begin(), twentyDifferentCircles.end ()));
+	
 }
 
 int main(int argc, char *argv[])
